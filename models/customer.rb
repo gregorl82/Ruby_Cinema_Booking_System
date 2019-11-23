12f39f6f91c0857a0b35aa30a1eb36601a4b9eec
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-23 11:52:45 +0000

    Adds all method to Customer class, tested in pry

--- models/customer.rb
+++ models/customer.rb
@@ -6,7 +6,7 @@ class Customer
   attr_reader :id
 
   def initialize(options)
-    @id = options['id'].to_i if options['id']
+    @id = options['id'].to_i() if options['id']
     @name = options['name']
     @funds = options['funds'].to_f
   end
@@ -16,6 +16,12 @@ class Customer
     values = [@name, @funds]
     result = SqlRunner.run(sql, values)
     @id = result[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM customers"
+    customers = SqlRunner.run(sql)
+    return customers.map {|customer| Customer.new(customer)}
   end
 
   def self.find_by_id(id)
