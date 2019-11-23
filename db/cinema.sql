456304ce834825f91ff19f41b0ee97c616b9a1e0
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-23 11:02:40 +0000

    Adds films schema

--- db/cinema.sql
+++ db/cinema.sql
@@ -1,7 +1,14 @@
 DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS films;
 
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
   name VARCHAR,
   funds DECIMAL(4,2)
+);
+
+CREATE TABLE films (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR,
+  price DECIMAL(4,2)
 );
