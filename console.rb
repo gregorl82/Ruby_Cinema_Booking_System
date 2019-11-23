fb528a1834f404cac2fd6a42cbc1e3e2a055835f
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-23 13:10:55 +0000

    Adds save method to Ticket class

--- console.rb
+++ console.rb
@@ -3,8 +3,10 @@ require_relative('models/customer.rb')
 require_relative('models/film.rb')
 require_relative('models/ticket.rb')
 
+Ticket.delete_all()
 Customer.delete_all()
 Film.delete_all()
+
 
 customer1 = Customer.new(
   {
--- models/ticket.rb
+++ models/ticket.rb
@@ -11,7 +11,10 @@ class Ticket
   end
 
   def save()
-
+    sql = "INSERT INTO tickets (film_id, customer_id) VALUES ($1, $2) RETURNING id"
+    values = [@film_id, @customer_id]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i()
   end
 
 end
