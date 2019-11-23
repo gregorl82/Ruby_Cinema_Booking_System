10de8e59bb2c0de9ac0e9665dbe7ac238b1d40e1
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-23 13:48:50 +0000

    Adds customers method to Film class, tested in pry

--- models/film.rb
+++ models/film.rb
@@ -31,6 +31,17 @@ class Film
     return Film.new(result)
   end
 
+  def customers()
+    sql = "SELECT customers.*
+      FROM customers
+      INNER JOIN tickets
+      ON customers.id = customer_id
+      WHERE film_id= $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    return customers.map {|customer| Customer.new(customer)}
+  end
+
   def update()
     sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
     values = [@title, @price, @id]
