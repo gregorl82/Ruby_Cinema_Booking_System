64b36dd542e26754766e30d35e35c631fe1ae476
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-23 14:28:12 +0000

    Updates save method in Ticket class to decrease the funds of the customer, tested in pry

--- models/ticket.rb
+++ models/ticket.rb
@@ -12,10 +12,29 @@ class Ticket
   end
 
   def save()
-    sql = "INSERT INTO tickets (film_id, customer_id) VALUES ($1, $2) RETURNING id"
-    values = [@film_id, @customer_id]
-    result = SqlRunner.run(sql, values)
-    @id = result[0]['id'].to_i()
+    # save ticket to tickets table and assign id
+    sql_ticket = "INSERT INTO tickets (film_id, customer_id) VALUES ($1, $2) RETURNING id"
+    values_ticket = [@film_id, @customer_id]
+    result_ticket = SqlRunner.run(sql_ticket, values_ticket)
+    @id = result_ticket[0]['id'].to_i()
+
+    # get film price from films table using film_id
+    sql_price = "SELECT * FROM films where id = $1"
+    values_price = [@film_id]
+    result_price = SqlRunner.run(sql_price, values_price)
+    price = result_price[0]['price'].to_f()
+
+    # get customer funds from customers table using customer_id
+    sql_funds = "SELECT * FROM customers where id = $1"
+    values_funds = [@customer_id]
+    result_funds = SqlRunner.run(sql_funds, values_funds)
+    funds = result_funds[0]['funds'].to_f()
+
+    # remove film price from customer funds and update customers table
+    funds -= price
+    sql_fund_update = "UPDATE customers SET funds = $1 WHERE id = $2"
+    values_fund_update = [funds, @customer_id]
+    SqlRunner.run(sql_fund_update, values_fund_update)
   end
 
   def self.all()
